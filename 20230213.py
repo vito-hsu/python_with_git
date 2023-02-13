03b2908103aff6dda81853ad3268bbcf6954c913
vito-hsu <garraypierce@gmail.com>
2023-02-13 15:17:10 +0800

    second commit

---
+++ 20230213.py
@@ -0,0 +1,11 @@
+# Hi~ I'm vito.
+# Today, I'm gonna introduce how to use git with vscode.
+# 1. Install git to your PC (not portable version if you want to use vscode)
+# 2. Click it, and it will link to your github, requesting login.
+# 3. In your vscode terminal, enter the following commands after you have checked your repository (ex. this work folder):
+#   3-1. git init
+#   3-2. git add .
+#   3-3. git commit -m "your commit"
+#   3-4. git remote add origin https://github.com/vito-hsu/python_with_git.git
+#   3-5. git push origin master
+# Let's try it
