65151ac6b35ed7366f00b5b875b63544896a7c5e
vito-hsu <garraypierce@gmail.com>
2023-02-13 16:06:03 +0800

    first branch commit

--- 20230213.py
+++ 20230213.py
@@ -11,3 +11,21 @@
 # Let's try it
 # I just run the above commands again, and you can see the .py file has changed like this.
 # Let's try it again.
+# You can see the 3-4 command doesn't need to run if you just rerun again.
+
+
+
+
+
+
+# In the second video about Git tutorial, I'll introduce the other common commands.
+# Before the pratice, we should know the purpose of branch in Git.
+# The key is that master means the main project.
+# But if we want to develop other projects, we should start other branches.
+# 1. git status                     --- it indicate the change/status about your current folder files 
+# 2. git log                        --- as you see, in our project, we push three times and this command will indicate the update time
+#                                       and commit messages
+# 3. git branch                     --- because we have not built branches in our project, it just indicate the "master"
+# 4. git branch branch_1            --- if you want to add new branch from the master
+# 5. git push origin branch_1
+# Let's try again.
